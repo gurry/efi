c32b8b6061977c43aa07428fd15a30d785163be5
Gurinder Singh <frederick.the.fool@gmail.com>
2018-02-28 15:43:06 +0530

    Made Debug implementation of a type more idiomatic. Should do this for all other types.

--- src/ffi/base.rs
+++ src/ffi/base.rs
@@ -62,7 +62,9 @@ pub union EFI_IP_ADDRESS {
 // Had to implement by hand 'cause Debug derive not allowed for unions
 impl fmt::Debug for EFI_IP_ADDRESS {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "EFI_IP_ADDRESS {{ Addr: {:?} }}", unsafe { self.Addr })
+        f.debug_struct("EFI_IP_ADDRESS")
+            .field("Addr", &format_args!("{:?}", unsafe { self.Addr }))
+            .finish()
     }
 }
 
