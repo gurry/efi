a0fe74df0c0e90b720b681c7c9e164f5dfcffdb1
Gurinder Singh <frederick.the.fool@gmail.com>
2018-03-28 16:34:34 +0530

    Removed some unused code

--- src/protocols/managed_network.rs
+++
@@ -1,6 +0,0 @@
-// use ffi::managed_network::EFI_MANAGED_NETWORK_CONFIG_DATA;
-
-// #[repr(C)]
-// pub struct ManagedNetworkConfigData(EFI_MANAGED_NETWORK_CONFIG_DATA); 
-// impl_wrapper!(ManagedNetworkConfigData, EFI_MANAGED_NETWORK_CONFIG_DATA); 
-
--- src/protocols/simple_network.rs
+++
@@ -1,5 +0,0 @@
-// use::ffi::simple_network::EFI_SIMPLE_NETWORK_MODE;
-
-// #[repr(C)]
-// pub struct SimpleNetworkMode(EFI_SIMPLE_NETWORK_MODE); 
-// impl_wrapper!(SimpleNetworkMode, EFI_SIMPLE_NETWORK_MODE); 
