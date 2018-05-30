f2426cad1844318c32e0794598090a15914d22b0
Gurinder Singh <frederick.the.fool@gmail.com>
2018-05-30 17:26:14 +0530

    Implemented Display for some types

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -55,7 +55,7 @@ use ffi::{
     ip4::EFI_IP4_MODE_DATA,
 };
 
-use core::{ptr, mem, ops::Drop};
+use core::{ptr, mem, ops::Drop, fmt};
 
 pub mod parser;
 
@@ -92,6 +92,13 @@ impl From<u32> for Ipv4Addr {
     }
 }
 
+impl fmt::Display for Ipv4Addr {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        let octets = self.octets();
+        write!(fmt, "{}.{}.{}.{}", octets[0], octets[1], octets[2], octets[3])
+    }
+}
+
 #[derive(Debug, Copy, Clone)]
 pub struct Ipv6Addr(EFI_IPv6_ADDRESS);
 
@@ -100,6 +107,20 @@ impl Ipv6Addr {
         Ipv6Addr(EFI_IPv6_ADDRESS {
             Addr: unsafe { mem::transmute([a, b, c, d, e, f, g, h]) } // Transmuting from an 8 elem array of u16 to 16 elem array of UINT8
         })
+    }
+
+    pub fn segments(&self) -> [u16; 8] {
+        let arr = &self.0.Addr;
+        [
+            (arr[0] as u16) << 8 | (arr[1] as u16),
+            (arr[2] as u16) << 8 | (arr[3] as u16),
+            (arr[4] as u16) << 8 | (arr[5] as u16),
+            (arr[6] as u16) << 8 | (arr[7] as u16),
+            (arr[8] as u16) << 8 | (arr[9] as u16),
+            (arr[10] as u16) << 8 | (arr[11] as u16),
+            (arr[12] as u16) << 8 | (arr[13] as u16),
+            (arr[14] as u16) << 8 | (arr[15] as u16),
+        ]
     }
 }
 
@@ -115,11 +136,92 @@ impl From<Ipv6Addr > for EFI_IPv6_ADDRESS {
     }
 }
 
+impl fmt::Display for Ipv6Addr {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self.segments() {
+            // We need special cases for :: and ::1, otherwise they're formatted
+            // as ::0.0.0.[01]
+            [0, 0, 0, 0, 0, 0, 0, 0] => write!(fmt, "::"),
+            [0, 0, 0, 0, 0, 0, 0, 1] => write!(fmt, "::1"),
+            // Ipv4 Compatible address
+            [0, 0, 0, 0, 0, 0, g, h] => {
+                write!(fmt, "::{}.{}.{}.{}", (g >> 8) as u8, g as u8,
+                       (h >> 8) as u8, h as u8)
+            }
+            // Ipv4-Mapped address
+            [0, 0, 0, 0, 0, 0xffff, g, h] => {
+                write!(fmt, "::ffff:{}.{}.{}.{}", (g >> 8) as u8, g as u8,
+                       (h >> 8) as u8, h as u8)
+            },
+            _ => {
+                fn find_zero_slice(segments: &[u16; 8]) -> (usize, usize) {
+                    let mut longest_span_len = 0;
+                    let mut longest_span_at = 0;
+                    let mut cur_span_len = 0;
+                    let mut cur_span_at = 0;
+
+                    for i in 0..8 {
+                        if segments[i] == 0 {
+                            if cur_span_len == 0 {
+                                cur_span_at = i;
+                            }
+
+                            cur_span_len += 1;
+
+                            if cur_span_len > longest_span_len {
+                                longest_span_len = cur_span_len;
+                                longest_span_at = cur_span_at;
+                            }
+                        } else {
+                            cur_span_len = 0;
+                            cur_span_at = 0;
+                        }
+                    }
+
+                    (longest_span_at, longest_span_len)
+                }
+
+                let (zeros_at, zeros_len) = find_zero_slice(&self.segments());
+
+                if zeros_len > 1 {
+                    fn fmt_subslice(segments: &[u16], fmt: &mut fmt::Formatter) -> fmt::Result {
+                        if !segments.is_empty() {
+                            write!(fmt, "{:x}", segments[0])?;
+                            for &seg in &segments[1..] {
+                                write!(fmt, ":{:x}", seg)?;
+                            }
+                        }
+                        Ok(())
+                    }
+
+                    fmt_subslice(&self.segments()[..zeros_at], fmt)?;
+                    fmt.write_str("::")?;
+                    fmt_subslice(&self.segments()[zeros_at + zeros_len..], fmt)
+                } else {
+                    let &[a, b, c, d, e, f, g, h] = &self.segments();
+                    write!(fmt, "{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}",
+                           a, b, c, d, e, f, g, h)
+                }
+            }
+        }
+    }
+}
+
 pub enum IpAddr {
     V4(Ipv4Addr),
     V6(Ipv6Addr)
 }
 
+impl fmt::Display for IpAddr {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            IpAddr::V4(ref a) => a.fmt(fmt),
+            IpAddr::V6(ref a) => a.fmt(fmt),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct SocketAddrV4 {
     ip: Ipv4Addr,
     port: u16,
@@ -139,6 +241,13 @@ impl SocketAddrV4 {
     }
 }
 
+impl fmt::Display for SocketAddrV4 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}:{}", self.ip(), self.port())
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub struct SocketAddrV6 {
     ip: Ipv6Addr,
     port: u16,
@@ -158,9 +267,25 @@ impl SocketAddrV6 {
     }
 }
 
+impl fmt::Display for SocketAddrV6 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]:{}", self.ip(), self.port())
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
 pub enum SocketAddr {
     V4(SocketAddrV4),
     V6(SocketAddrV6)
+}
+
+impl fmt::Display for SocketAddr {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            SocketAddr::V4(ref a) => a.fmt(f),
+            SocketAddr::V6(ref a) => a.fmt(f),
+        }
+    }
 }
 
 pub struct Tcp4Stream {
