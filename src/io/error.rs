f66d2cf66ba09a09403e6853ba9d2a9ef7ae076f
Gurinder Singh <gurinder.singh@1e.com>
2023-01-13 08:35:31 +0100

    Fixed warnings about box keyword being experimental

--- src/io/error.rs
+++ src/io/error.rs
@@ -508,12 +508,12 @@ mod test {
         let msg = error_string(code);
         let kind = decode_error_kind(code);
         let err = Error {
-            repr: Repr::Custom(box Custom {
+            repr: Repr::Custom(Box::new(Custom {
                 kind: ErrorKind::InvalidInput,
-                error: box Error {
+                error: Box::new(Error {
                     repr: super::Repr::Os(code)
-                },
-            })
+                }),
+            }))
         };
         let expected = format!(
             "Custom {{ \
