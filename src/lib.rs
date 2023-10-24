68fe70284d5db95d6d9412d621e9a0c0ee95c3a2
Gurinder Singh <frederick.the.fool@gmail.com>
2023-10-24 08:32:50 +0530

    Removed some unnecessary comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-// TODO: Can we use core::intrinsics (https://doc.rust-lang.org/1.12.0/core/intrinsics/fn.copy.html) instead of compiler_builtins crate?
-// TODO: At many places in this crate we're not handling errors well. We're swalling inner errors such as EFI_STATUSes. Fix this situation.
+// TODO: At many places in this crate we're not handling errors well. We're swallowing inner errors such as EFI_STATUSes. Fix this situation.
 #![no_std]
 #![recursion_limit="100"] // Needed for the dns module (because it does recursive name resolution)
-
-// #![warn(missing_debug_implementations)]
 
 #[macro_use] extern crate alloc;
 
